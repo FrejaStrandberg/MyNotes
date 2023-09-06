9d72ec0b58454fb703e259ee4d7a3f6b6bd7ec60
FrejaStrandberg <frej5244@mail.hansenberg.dk>
2023-09-06 08:52:35 +0200

    Ændringer

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
+Page Firstpage = new Page("Min første side i min awesome bog");
+
+Page Secondpage = new Page("Min første side i min awesome bog");
+
+Page Thirdpage = new Page("Min første side i min awesome bog");
+
+Firstpage.Read();
+
+Chapter Kapitel = new Chapter("Variabler");
+
+
+
+Console.ReadLine();
