45095b628fbf4eaadbc7334086b10681895e7232
FrejaStrandberg <frej5244@mail.hansenberg.dk>
2023-09-06 09:19:44 +0200

    Definitioner på variabler

--- MyNotes.cs
+++ MyNotes.cs
@@ -15,16 +15,14 @@ Observe:
  bool TooCoolForSchool = false;
 ";
 
-    public static string VariablesPageTwo =
-@"...
-So often used without consideration for its magnificence, the humble 'variable' is the heart of computation. 
-It can be used to store and manipulate data, and is the backbone of all computer programs. 
-The 'variable' is a named container for a value, and can be of different types. 
-The type of a variable determines what kind of value it can hold, and what kind of operations can be performed on it. 
-There are many different types of variables, but the most common ones are 'int', 'string', 'float', and 'bool'. 
-An 'int' is a whole number, like 1, 2, 3, 4, 5, 6, 7, 8, 9, 0. 
-A 'string' is a sequence of characters, like ""Hello World"", ""I am a string"", and ""C# is awesome"". 
-A 'float' is a decimal number, like 1.234, 5.678, and 9.101112. 
-A 'bool' is a true or false value, like true and false. ";
+    public static string OurIntPage =@"Int er en forkortelse for 'integer,' som på dansk oversættes til 'heltal.'
+I programmering refererer 'int' normalt til en datatypen, der bruges til at repræsentere heltal, dvs. positive og negative hele tal uden decimaler.
+Heltal kan omfatte både positive tal som 1, 2, 3 og negative tal som -1, -2, -3 samt nul (0).";
 
+ public static string OurFloatPage =@"Float er en forkortelse af floating-point-number. Det
+repræsenterer kommatal/decimaltal";
+
+public static string OurStringPage =@"string bruges til at repræsentere tekst, der både kan indeholde bogstaver, tal, tegn og mellemrum.";
+
+public static string OurBoolPage =@"Bool fortælle rom noget enten er sandt eller falskst. Den kan f.eks. sige om en variabel er størrer end et andet, om en knap er blevet trykket eller om en handling er blevet fundført med succes.";
 }
